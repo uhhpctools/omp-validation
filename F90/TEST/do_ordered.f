b65a5120ebd78f439ec2a26fc5d37a3cb1e59d77
deepak <deepak>
2006-07-31 16:58:32 +0000

    Reverting do_ordered.f to older version. Added flush command to preCompilatino, and changed output to match that in C Validation.

--- F90/TEST/do_ordered.f
+++ F90/TEST/do_ordered.f
@@ -23,13 +23,11 @@
         sum = 0
         is_larger = 1
         last_i = 0
-        write(*,*) "before parallel"
 !$omp parallel private(my_islarger)
-        write(*,*) "in parallel"
         my_islarger = 1
 !$omp do schedule(static,1) ordered
         do i=1, 99
-!$omp ordered        
+!$omp ordered
           if ( chk_i_islarger(i) .eq. 1 .and. my_islarger .eq. 1) then
             my_islarger = 1
           else
@@ -39,7 +37,6 @@
 !$omp end ordered
         end do
 !$omp end do
-!        write(*,*) "After ordered loop"
 !$omp critical
         if (is_larger .eq. 1 .and. my_islarger .eq. 1 ) then
           is_larger = 1
